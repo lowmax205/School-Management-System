b6619bd714d01e0a1e91cd4da25e30f197480956
Nilo Jr. - LowmaxPC <nilojr.olang@gmail.com>
2024-12-05 06:50:09 +0800

    update

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
                 <div class="card h-100">
                     <div class="card-body">
                         <h3 class="card-title">Vision</h3>
-                        <p>SNSU as a leading institution of higher learning in human development, science and technology, and environmental sustainability in the Caraga Region</p>
+                        <p>A leading industry-driven State University.</p>
                     </div>
                 </div>
             </div>
@@ -26,7 +26,23 @@
                 <div class="card h-100">
                     <div class="card-body">
                         <h3 class="card-title">Mission</h3>
-                        <p>SNSU shall provide quality education through responsive instruction, distinctive research, sustainable extension, and production services for improved quality of life towards nation building.</p>
+                        <p>To provide quality and inclusive education; establish industry and business innovation infrastructures; develop environmental initiatives; promote peace, justice and transformational leadership; and build strong and dynamic partnership with stakeholders.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h3 class="card-title">GOALS AND OBJECTIVES</h3>
+                        <p>SNSU aims to produce quality graduates that respond to the dynamics of national and international standards. Its goals and objectives are focused on the 5-point agenda: Instruction, Research Innovation and Extension (RIE), Resources Generation, Policy Implementation and Good Governance.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h3 class="card-title">INSTITUTIONAL CORE VALUES</h3>
+                        <p>SNSU is guided by its institutional core values: Integrity, Competence, and Respect </p>
                     </div>
                 </div>
             </div>
